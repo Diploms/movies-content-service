12f745d0a6911be5a40722fa34a0e28c53c1c14d
Dmytro Ivashchenko <gen.style22@gmail.com>
2025-01-09 13:05:32 +0200

    Initial commit

---
+++ src/main/java/ua/karazin/moviescontentservice/MoviesContentServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.karazin.moviescontentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesContentServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MoviesContentServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ua/karazin/moviescontentservice/TestMoviesContentServiceApplication.java
@@ -0,0 +1,12 @@
+package ua.karazin.moviescontentservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestMoviesContentServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.from(MoviesContentServiceApplication::main)
+        .with(TestcontainersConfiguration.class).run(args);
+  }
+
+}
